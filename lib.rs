c9a6205d19fe3ce2b96182f6625300d25fb5df24
hgminerva <hgminerva@gmail.com>
2025-11-10 20:16:07 +0800

    Adding draws

--- lib.rs
+++ lib.rs
@@ -2,7 +2,6 @@
 
 #[ink::contract]
 mod lottery {
-    use ink::storage::Mapping;
 
     /// Lottery error messages
     #[derive(scale::Encode, scale::Decode, Clone, Debug, PartialEq, Eq)]
@@ -10,57 +9,161 @@ mod lottery {
     pub enum LotteryError {
         AlreadyStarted,
         StartingBlockPassed,
+        BadOrigin,
+        TooManyDraws,
     }
 
     /// Lottery Setup 
-    #[derive(scale::Encode, scale::Decode, Clone, Debug, PartialEq, Eq, Default)]
+    #[derive(scale::Encode, scale::Decode, Clone, Debug, PartialEq, Eq)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout))]
     pub struct LotterySetup {
         starting_block: u32,
-        daily_total_blocks: u32,
+        daily_total_blocks: u16,
         last_draw_number: u32,
+        maximum_draws: u8,
+        maximum_bets: u16,
         is_started: bool,
+    }
+
+    /// Bet
+    #[derive(scale::Encode, scale::Decode, Clone, Debug, PartialEq, Eq)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout))]
+    pub struct Bet {
+        bettor: AccountId,
+        bet_number: u16,
+    }
+
+    /// Winner
+    #[derive(scale::Encode, scale::Decode, Clone, Debug, PartialEq, Eq)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout))]
+    pub struct Winner {
+        bettor: AccountId,
+        winning_amount: Balance,
+    }
+
+    /// Draw meta data 
+    #[derive(scale::Encode, scale::Decode, Clone, Debug, PartialEq, Eq, Default)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout))]
+    pub struct Draw {
+        draw_number: u32,
+        block_interval: u16,
+        jackpot: Balance,
+        bets: Vec<Bet>,
+        winning_number: u16,
+        winners: Vec<Winner>,
+        is_open: bool,
     }
 
     /// Lottery
     #[ink(storage)]
     pub struct Lottery {
+        operator: AccountId,
         lottery_setup: LotterySetup,
+        draws: Vec<Draw>,
     }
 
-    /// Draw(s)
-    #[ink::storage_item]
-    pub struct Draw {
-        draw_number: u32,
-        jackpot: Balance,
-        bets: Mapping<AccountId, Balance>,
-        is_open: bool,
-    }
-
+    /// Implementation
     impl Lottery {
+
         /// Constructor
         #[ink(constructor)]
-        pub fn new(init_start: bool) -> Self {
+        pub fn new(starting_block: u32,
+                   daily_total_blocks: u16,
+                   last_draw_number: u32,
+                   maximum_draws: u8,
+                   maximum_bets: u16,
+                   init_start: bool) -> Self 
+        {
+            let caller = Self::env().caller();
             Self { 
+                operator: caller,
                 lottery_setup: LotterySetup {
-                    starting_block: 0u32,
-                    daily_total_blocks: 14_400u32,
-                    last_draw_number: 0u32,
+                    starting_block: starting_block,
+                    daily_total_blocks: daily_total_blocks,
+                    last_draw_number: last_draw_number,
+                    maximum_draws: maximum_draws,
+                    maximum_bets: maximum_bets,
                     is_started: init_start, 
-                }
+                },
+                draws: Vec::new(),
             }
         }
 
         /// Make a default instantiation
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self::new(false)
+            Self::new(0u32,
+                      14_400u16,
+                      0u32,
+                      2u8,
+                      1_000u16,
+                      false)
         }
+
+        /// Setup the lottery
+        #[ink(message)]
+        pub fn setup(&mut self, 
+                     starting_block: u32,
+                     daily_total_blocks: u16,
+                     maximum_draws: u8,
+                     maximum_bets: u16) -> Result<(), LotteryError> {
+
+            if self.env().caller() != self.operator {
+                return Err(LotteryError::BadOrigin);
+            } 
+
+            self.lottery_setup.starting_block = starting_block;
+            self.lottery_setup.daily_total_blocks = daily_total_blocks;
+            self.lottery_setup.maximum_draws = maximum_draws;
+            self.lottery_setup.maximum_bets = maximum_bets;
+
+            Ok(())
+        }
+
+        /// Add draw
+        #[ink(message)]
+        pub fn add_draw(&mut self, block_interval: u16) -> Result<(), LotteryError>  {
+            // Only the operator can add a draw
+            if self.env().caller() != self.operator {
+                return Err(LotteryError::BadOrigin);
+            } 
+
+            // Must not exceed the maximum number of draws setup in the lottery
+            if self.draws.len() >= self.lottery_setup.maximum_draws.into() {
+                return Err(LotteryError::TooManyDraws);
+            }
+
+            let next_draw_number = self.draws
+                                            .iter()
+                                            .map(|d| d.draw_number)
+                                            .max()
+                                            .unwrap_or(0)
+                                            .saturating_add(1);
+
+            let new_draw = Draw {
+                draw_number: next_draw_number,
+                block_interval: block_interval,
+                jackpot: 0,
+                bets: Vec::new(),
+                winning_number: 0,
+                winners: Vec::new(),
+                is_open: false,
+            };
+
+            self.draws.push(new_draw);
+
+            Ok(())
+        }
+
 
         /// Start the lottery
         #[ink(message)]
         pub fn start(&mut self) -> Result<(), LotteryError> {
-            let current_block = self.env().block_number();
+            let current_block: u32 = self.env().block_number();
+
+            if self.env().caller() != self.operator {
+                return Err(LotteryError::BadOrigin);
+            } 
 
             if self.lottery_setup.is_started {
                 return Err(LotteryError::AlreadyStarted);
@@ -71,13 +174,20 @@ mod lottery {
             }
 
             self.lottery_setup.is_started = true;
+
             Ok(())
         }
 
         /// Stop the lottery
         #[ink(message)]
-        pub fn stop(&mut self) {
+        pub fn stop(&mut self) -> Result<(), LotteryError> {
+            if self.env().caller() != self.operator {
+                return Err(LotteryError::BadOrigin);
+            } 
+
             self.lottery_setup.is_started = false;
+
+            Ok(())
         }
 
         /// Returns lottery setup
@@ -94,7 +204,6 @@ mod lottery {
     mod tests {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
-        use ink::env;
 
         /// We test if the default constructor does its job.
         #[ink::test]
@@ -102,49 +211,78 @@ mod lottery {
             let lottery = Lottery::default();
             let lottery_setup = LotterySetup {
 				starting_block: 0u32,
-                daily_total_blocks: 14_400u32,
-                last_draw_number: 032,
-                is_started: false,
-			};
-            assert_eq!(lottery.get_lottery_setup(), lottery_setup);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut lottery = Lottery::new(false);
-            let lottery_setup = LotterySetup {
-				starting_block: 0u32,
-                daily_total_blocks: 14_400u32,
+                daily_total_blocks: 14_400u16,
                 last_draw_number: 0u32,
+                maximum_draws: 2u8,
+                maximum_bets: 1_000u16,
                 is_started: false,
-			};
-            assert_eq!(lottery.get_lottery_setup(), lottery_setup);
-            let _ = lottery.start();
-            let lottery_setup = LotterySetup {
-				starting_block: 0u32,
-                daily_total_blocks: 14_400u32,
-                last_draw_number: 0u32,
-                is_started: true,
 			};
             assert_eq!(lottery.get_lottery_setup(), lottery_setup);
         }
 
         #[ink::test]
         fn start_lottery_works() {
-            let mut lottery = Lottery::new(false);
+            let mut lottery = Lottery::new(0u32,
+                                                    14_400u16,
+                                                    0u32,
+                                                    2u8,
+                                                    1_000u16,
+                                                    false);
             let _ = lottery.start();
             let result = lottery.start();
             assert!(matches!(result, Err(LotteryError::AlreadyStarted)));       
         }    
+
+        #[ink::test]
+        fn setup_lottery_works() {
+            let mut lottery = Lottery::new(0u32,
+                                                    14_400u16,
+                                                    0u32,
+                                                    2u8,
+                                                    1_000u16,
+                                                    false);
+            let _ = lottery.start();
+            let _ = lottery.setup(1_000_000u32, 14_400u16, 2u8, 1_000u16);
+
+            let lottery_setup = LotterySetup {
+				starting_block: 1_000_000u32,
+                daily_total_blocks: 14_400u16,
+                last_draw_number: 0u32,
+                maximum_draws: 2u8,
+                maximum_bets: 1_000u16,
+                is_started: true,
+			};
+            assert_eq!(lottery.get_lottery_setup(), lottery_setup);
+        }
+
+        #[ink::test]
+        fn adding_draw_works() {
+            let mut lottery = Lottery::new(0u32,
+                                                    14_400u16,
+                                                    0u32,
+                                                    2u8,
+                                                    1_000u16,
+                                                    false);
+            
+            let _ = lottery.add_draw(1_000u16);
+            assert_eq!(lottery.draws.len(), 1);
+            
+            let new_draw = Draw {
+                draw_number: 1,
+                block_interval: 1_000u16,
+                jackpot: 0,
+                bets: Vec::new(),
+                winning_number: 0,
+                winners: Vec::new(),
+                is_open: false,
+            };
+            assert_eq!(lottery.draws[0], new_draw);
+        }
+
     }
 
 
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
+    /// End-to-end (E2E) or integration tests for ink! contracts.
     #[cfg(all(test, feature = "e2e-tests"))]
     mod e2e_tests {
         /// Imports all the definitions from the outer scope so we can use them here.
