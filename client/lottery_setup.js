2a8fa86606fbb43eeffd010856d3eb5b54ad003f
hgminerva <hgminerva@gmail.com>
2025-11-10 22:55:47 +0800

    Added client (javascript)

---
+++ client/lottery_setup.js
@@ -0,0 +1,30 @@
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { ContractPromise } from "@polkadot/api-contract";
+import { Keyring } from "@polkadot/keyring";
+import fs from "fs";
+import 'dotenv/config';
+
+const WS_ENDPOINT = process.env.WS_ENDPOINT;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CONTRACT_ABI_PATH = process.env.CONTRACT_ABI_PATH;
+const ACCOUNT = process.env.ACCOUNT;
+
+
+console.log("Connecting to blockchain...");
+const wsProvider = new WsProvider(WS_ENDPOINT);
+const api = await ApiPromise.create({ provider: wsProvider });
+console.log("Connected to:", (await api.rpc.system.chain()).toHuman());
+
+const abiJSON = JSON.parse(fs.readFileSync(CONTRACT_ABI_PATH, "utf8"));
+const contract = new ContractPromise(api, abiJSON, CONTRACT_ADDRESS);
+
+const keyring = new Keyring({ type: "sr25519" });
+const alice = keyring.addFromUri(ACCOUNT);
+
+console.log("Reading lottery setup");
+const { output } = await contract.query.getLotterySetup(
+    alice.address,
+    { gasLimit: -1, storageDepositLimit: null }
+);
+
+console.log("Lottery setup:", output?.toHuman());
